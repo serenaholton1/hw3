13f4b0167cb3a966dda6ecd2c85a414cbb775333
serenaholton1 <serena.holton@kellogg.northwestern.edu>
2026-02-23 02:37:48 +0000

    updating routes file to remove redundant lines

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   resources "places"
   resources "entries"
 
-  get("/places", { :controller => "places", :action => "index" })
-  get("/places/new", { :controller => "places", :action => "new" })
-  post("/places", { :controller => "places", :action => "create" })
-  get("/places/:id", { :controller => "places", :action => "show" })
+  #get("/places", { :controller => "places", :action => "index" })
+  #get("/places/new", { :controller => "places", :action => "new" })
+  #post("/places", { :controller => "places", :action => "create" })
+  #get("/places/:id", { :controller => "places", :action => "show" })
 
-  get("/entries/new", { :controller => "entries", :action => "new" })
-  post("/entries", { :controller => "entries", :action => "create" })
+  #get("/entries/new", { :controller => "entries", :action => "new" })
+  #post("/entries", { :controller => "entries", :action => "create" })
 end
